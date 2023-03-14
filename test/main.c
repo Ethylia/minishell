f79a31d9812539c4f029808cb1af56dcb0a23e36
François Martineau <ffrancoismmartineau@gmail.com>
2023-03-14 15:44:05 -0400

    test pipe 2

--- test/main.c
+++ test/main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: francoma <francoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/14 09:17:23 by francoma          #+#    #+#             */
-/*   Updated: 2023/03/14 10:28:26 by francoma         ###   ########.fr       */
+/*   Created: 2023/03/14 14:30:34 by francoma          #+#    #+#             */
+/*   Updated: 2023/03/14 15:43:45 by francoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,74 +14,65 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <stdio.h>
 
 #define ERROR -1
 #define SUCCESS 0
+#define NUM_CMD 3
 
-// fork 1st, redir out, close in
-// fork next, redir in out
-// last, redir in, close out
-// flags: pipeline first, pipeline last
-int	make_pipe(int pipefd[2], int pid[2])
+typedef struct s_cmd
 {
-	if (pipe(pipefd) == ERROR)
+	pid_t		pid;
+	const char	*path;
+	char *const	*argv;
+}	t_cmd;
+
+static int	is_pipeline_start(int *ppipefd)
+{
+	return (ppipefd == NULL);
+}
+
+static int	is_pipeline_end(t_cmd *cmds)
+{
+	return (cmds[1].path == NULL);
+}
+
+int	make_pipe(int ppipefd[2], t_cmd *cmds)
+{
+	int	pipefd[2];
+	
+	if (!is_pipeline_end(cmds)
+		&& pipe(pipefd) == ERROR)
 		return (ERROR);
-	pid[0] = fork();
-	if (pid[0] == ERROR)
+	cmds[0].pid = fork();
+	if (cmds[0].pid == 0)
 	{
-		close(pipefd[0]);
-		close(pipefd[1]);
+		if (!is_pipeline_start(ppipefd))
+		{
+			close(ppipefd[1]);
+			dup2(ppipefd[0], STDIN_FILENO);
+		}			
+		if (!is_pipeline_end(cmds))
+		{
+			close(pipefd[0]);
+			dup2(pipefd[1], STDOUT_FILENO);
+		}
+		execve(cmds[0].path, cmds[0].argv, NULL);
 		return (ERROR);
 	}
-	else if (pid[0] == 0)
-	{
-		close(pipefd[0]);
-		dup2(pipefd[1], STDOUT_FILENO);
-		return (SUCCESS);
-	}
-	pid[1] = fork();
-	if (pid[1] == ERROR)
-	{
-		kill(SIGKILL, pid[0]);
-		close(pipefd[0]);
-		close(pipefd[1]);
-		return (ERROR);
-	}
-	else if (pid[1] == 0)
-	{
-		close(pipefd[1]);
-		dup2(pipefd[0], STDIN_FILENO);
-		return (SUCCESS);
-	}
+	if (!is_pipeline_end(cmds))
+		return (make_pipe(pipefd, cmds + 1));
 	return (SUCCESS);
 }
 
 int main(void)
 {
-	int pipefd[2];
-	int pid[2];
+	t_cmd	cmds[3];
 	
-	if (make_pipe(pipefd, pid) == ERROR)
+	cmds[0] = (t_cmd){.path = "/bin/cat", .argv = (char *const []){"/bin/cat", NULL}};
+	cmds[1] = (t_cmd){.path = "/usr/bin/grep", .argv = (char *const []){"/usr/bin/grep", "salut", NULL}};
+	cmds[2] = (t_cmd){.path = NULL};
+	if (make_pipe(NULL, cmds) == ERROR)
 		return (EXIT_FAILURE);
-	if (pid[0] == 0)
-	{
-		if (execve("/bin/cat", (char *const []){"/bin/cat", NULL}, (char *const []){"", NULL})
-			== ERROR)
-		{
-			kill(SIGKILL, pid[1]);
-			return (EXIT_FAILURE);
-		}
-	}
-	else if (pid[1] == 0)
-	{
-		if (execve("/usr/bin/grep", (char *const []){"/usr/bin/grep", "salut", NULL}, (char *const []){"", NULL})
-			== ERROR)
-		{
-			kill(SIGKILL, pid[0]);
-			return (EXIT_FAILURE);
-		}
-	}
-	while (waitpid(-1, NULL, 0) != -1)
-		;
 	return (EXIT_SUCCESS);
 }
