206fdf9ad48d37dd5dff88204c770a30c27f99a4
Elijah Boyce-Nadeau <eboyce-n@c1r6p5.42quebec.com>
2023-03-14 13:50:05 -0400

    nextarg to unify quote handling

--- src/util/str3.c
+++ src/util/str3.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   str3.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: francoma <francoma@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eboyce-n <eboyce-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 17:32:40 by francoma          #+#    #+#             */
-/*   Updated: 2023/03/13 17:33:48 by francoma         ###   ########.fr       */
+/*   Updated: 2023/03/14 13:49:41 by eboyce-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,26 @@ int	starts_with(const char *s, const char *prefix)
 
 	len = strln(prefix);
 	return (strncmp(s, prefix, len));
+}
+
+size_t	nextarg(const char *s)
+{
+	size_t	i;
+	int		quote;
+
+	i = -1;
+	quote = 0;
+	while (s[++i])
+	{
+		if (s[i] == '\'' || s[i] == '"')
+		{
+			if (!quote)
+				quote = s[i];
+			else if (quote == s[i])
+				quote = 0;
+		}
+		else if (whitespace(s[i]) && !quote)
+			break ;
+	}
+	return (i);
 }
