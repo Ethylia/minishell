3c8e82190153ec2ca006dc5cd42b66befc8bda09
Elijah Boyce-Nadeau <eboyce-n@c1r6p5.42quebec.com>
2023-03-14 13:51:36 -0400

    oopsie woopsie

--- src/util/util.h
+++ src/util/util.h
@@ -6,7 +6,7 @@
 /*   By: eboyce-n <eboyce-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 15:21:33 by francoma          #+#    #+#             */
-/*   Updated: 2023/03/14 09:33:21 by eboyce-n         ###   ########.fr       */
+/*   Updated: 2023/03/14 13:51:22 by eboyce-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int		strncmp(const char *s1, const char *s2, size_t n);
 int		strcmp_del(const char *s1, const char *s2, const char del);
 char	*concatstr(size_t n, ...);
 int		whitespace(char c);
+size_t	nextarg(const char *s);
 
 int		starts_with(const char *s, const char *prefix);
 
