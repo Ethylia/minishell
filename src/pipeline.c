44f3d0510aceb44ff11ea2270716978ef758437a
François Martineau <ffrancoismmartineau@gmail.com>
2023-03-27 14:29:52 -0400

    refactor pipeline exit error return

--- src/pipeline.c
+++ src/pipeline.c
@@ -6,7 +6,7 @@
 /*   By: francoma <francoma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 08:46:35 by francoma          #+#    #+#             */
-/*   Updated: 2023/03/24 11:22:38 by francoma         ###   ########.fr       */
+/*   Updated: 2023/03/24 15:53:41 by francoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,55 +35,55 @@ static void	close_pipe(t_pipe *p)
 	close(p->write);
 }
 
-static int	exit_err(char *exec_path)
+static void	exit_err(char *exec_path)
 {
 	if (exec_path)
 		print_err(exec_path);
 	free(exec_path);
-	return (ERROR);
+	freedata();
+	exit(EXIT_FAILURE);
 }
 
-static int	exit_notfound(char *exec_path)
+static void	exit_notfound(char *exec_path)
 {
 	char	*msg;
 
+	freedata();
 	msg = concatstr(4, NAME, ": ", exec_path, ": command not found\n");
-	if (!msg)
-	{
-		free(exec_path);
-		return (ERROR);
-	}
-	write(STDERR_FILENO, msg, strln(msg));
 	free(exec_path);
+	if (!msg)
+		exit(EXIT_FAILURE);
+	write(STDERR_FILENO, msg, strln(msg));
 	free(msg);
-	return (ERROR);
+	exit(EXIT_FAILURE);
 }
 
 // cmd not found: currently displays "file or dir not found"
-static int	exec_cmd(t_cmd *cmd, t_pipe *prev_pipe, t_pipe *next_pipe)
+static void	exec_cmd(t_cmd *cmd, t_pipe *prev_pipe, t_pipe *next_pipe)
 {
 	char		*exec_path;
 	const char	*p;
 
 	if (redir_input(cmd, prev_pipe) == ERROR
 		|| redir_output(cmd, next_pipe) == ERROR)
-		return (exit_err(NULL));
+		exit_err(NULL);
 	if (is_builtin(cmd))
 	{
 		if (exec_builtin(cmd) == ERROR)
-			return (exit_notfound(NULL));
+			exit_notfound(NULL);
+		freedata();
 		exit(EXIT_SUCCESS);
 	}
 	exec_path = resolve_exec_path(cmd->argv[0]);
 	if (!exec_path)
-		return (exit_err(NULL));
+		exit_err(NULL);
 	execve(exec_path, cmd->argv, getdata()->exported_env);
 	p = get_var(getdata()->local_env, "PATH");
 	if (!p)
 		p = get_var(getdata()->exported_env, "PATH");
 	if (p && p[0] && !strchar(cmd->argv[0], '/'))
-		return (exit_notfound(exec_path));
-	return (exit_err(exec_path));
+		exit_notfound(exec_path);
+	exit_err(exec_path);
 }
 
 int	pipeline(t_cmd *cmd, t_pipe *prev_pipe)
@@ -95,8 +95,8 @@ int	pipeline(t_cmd *cmd, t_pipe *prev_pipe)
 	if (cmd->pipecmd && pipe(next_pipe.pipe) == ERROR)
 		return (ERROR);
 	cmd_pid = fork();
-	if (cmd_pid == 0 && exec_cmd(cmd, prev_pipe, &next_pipe) == ERROR)
-		exit(EXIT_FAILURE);
+	if (cmd_pid == 0)
+		exec_cmd(cmd, prev_pipe, &next_pipe);
 	if (prev_pipe)
 		close_pipe(prev_pipe);
 	if (cmd->pipecmd)
