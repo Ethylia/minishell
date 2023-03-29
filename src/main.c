ffe0d26727ee2e60efb370c69db9bbb6ae4437eb
Elijah Boyce-Nadeau <eboyce-n@c1r6p5.42quebec.com>
2023-03-29 14:20:11 -0400

    woo

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: eboyce-n <eboyce-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 07:57:50 by eboyce-n          #+#    #+#             */
-/*   Updated: 2023/03/28 16:06:42 by eboyce-n         ###   ########.fr       */
+/*   Updated: 2023/03/29 14:19:51 by eboyce-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,10 +65,10 @@ t_token	*nextcmd(t_token *token, int stat)
 		return (next);
 	nestlvl = next->nestlvl;
 	if (stat != 0)
-		while (next->type == tand || next->nestlvl > nestlvl)
+		while (next->type && (next->type == tand || next->nestlvl > nestlvl))
 			next = findnext(next + 1, tand | tor);
 	else if (stat == 0)
-		while (next->type == tor || next->nestlvl > nestlvl)
+		while (next->type && (next->type == tor || next->nestlvl > nestlvl))
 			next = findnext(next + 1, tand | tor);
 	if (next->type)
 		++next;
