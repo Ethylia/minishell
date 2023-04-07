45b5dcc94f7168db8c9e0e651a6ffd249115e64d
Elijah Boyce-Nadeau <eboyce-n@c1r6p5.42quebec.com>
2023-04-07 13:30:13 -0400

    woo

--- src/sig.c
+++ src/sig.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sig.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: francoma <francoma@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eboyce-n <eboyce-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 09:05:19 by francoma          #+#    #+#             */
-/*   Updated: 2023/04/05 11:06:23 by francoma         ###   ########.fr       */
+/*   Updated: 2023/04/07 13:11:22 by eboyce-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,10 @@
 
 static void	sig_handler(int signo)
 {
-	if (signo == SIGINT && isatty(STDOUT_FILENO))
+	if (signo == SIGINT && isatty(STDOUT_FILENO) && !rl_already_prompted)
 	{
+		while (waitpid(-1, NULL, 0) != NO_CHILD_LEFT)
+			;
 		rl_replace_line("", 0);
 		write(1, "\n", 1);
 		rl_on_new_line();
